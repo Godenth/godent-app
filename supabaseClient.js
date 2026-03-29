ab29807a996749bee4aa617724c8af30dac19060
Godenth <contato.idealilab@gmail.com>
2026-03-28 21:25:15 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://eziwvufunacyxcxejgje.supabase.co'
 
 // APAGUE O TEXTO ABAIXO E COLE A SUA CHAVE ANON (aquela que começa com sb_...)
-const supabaseAnonKey = 'SUA_CHAVE_AQUI'
+const supabaseAnonKey = 'sb_publishable_du7opJxHeqGDEuTDlbrLoA_NJBrThSx'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
